2ce41d9eab0d2a7d66035c14b4e2a6e3de43457c
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-28 19:40:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,17 @@
 def addition(num1, num2)
-  puts num1 + num2
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-  puts num1 - num2
+  return num1 - num2
 end
 
 def division(num1, num2)
-  puts num1 / num2
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
-  puts num * num
+  return num * num
 end
 
 def modulo(num1, num2)
